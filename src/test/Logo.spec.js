b37a8e28b4536e0bbe5861fa56ee88094d70a37f
Raffaello Damgaard <raffacabofrio@gmail.com>
2020-06-28 19:22:40 -0300

    Remove unwanted files.

--- src/test/Logo.spec.js
+++ src/test/Logo.spec.js
@@ -1,9 +1,11 @@
 import { mount } from '@vue/test-utils'
 import Logo from '@/components/Logo.vue'
 
-describe('Logo', () => {
-  test('is a Vue instance', () => {
-    const wrapper = mount(Logo)
-    expect(wrapper.vm).toBeTruthy()
-  })
-})
+// TODO: add unity tests
+
+// describe('Logo', () => {
+//   test('is a Vue instance', () => {
+//     const wrapper = mount(Logo)
+//     expect(wrapper.vm).toBeTruthy()
+//   })
+// })
