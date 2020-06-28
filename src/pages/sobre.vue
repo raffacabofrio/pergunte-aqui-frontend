98e1e1d9d7fa51b75798364633a0f968b6e8bb08
Raffaello Damgaard <raffacabofrio@gmail.com>
2020-06-28 16:39:49 -0300

    Add about page.

---
+++ src/pages/sobre.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-layout>
+    <v-flex class="text-left" style="max-width=600px !important">
+      <h1>Sobre nosso app</h1>
+      <p>App prova de conceito para perguntas e respostas. É um projeto simples, fork do Sharebook, para mostrar conceitos importantes no desenvolvimento de software.
+        <ul>
+          <li>Simplicidade.</li>
+          <li>Robustez.</li>
+          <li>Arquitetura DDD.</li>
+          <li>Histórico do git em sequência lógica e educativa.</li>
+        </ul>
+      </p>
+
+      <h2>Backend</h2>
+      <ul>
+        <li>.Net Core 3.1</li>
+        <li>Sql Server</li>
+        <li>Entity Framework</li>
+        <li><a href="https://github.com/raffacabofrio/pergunte-aqui-backend" target="_blank" rel="noopener noreferrer">Github</a></li>
+      </ul>
+
+      <h2>Frontend</h2>
+      <ul>
+        <li>Vue/ Nuxt</li>
+        <li>Material Design/ Vuetify</li>
+        <li>Prettier auto-formatter</li>
+        <li><a href="https://github.com/raffacabofrio/pergunte-aqui-frontend" target="_blank" rel="noopener noreferrer">Github</a></li>
+      </ul>
+
+      <h1>Sobre o programador</h1>
+      <p>
+        <img src="raffa.jpeg" alt="" align="left">
+        Olá, meu nome é Raffaello e sou desenvolvedor de Software. Trabalho exclusivamente nessa área há quase 20 anos. 
+        Já participei de todas as fases do ciclo de vida do software. Desde a
+        especificação, ao desenvolvimento ágil até a entrega e sustentação.
+        Me considero criativo e entusiasta de novas tecnologias e com boas habilidades de comunicação.
+        Busco usar o meu perfil empreendedor para exercer uma influência positiva e engajamento dos
+        times.<br><br>
+        <v-btn color="primary" href="https://www.linkedin.com/in/raffacabofrio/" target="_blank">Fale comigo no linkedin</v-btn>
+      </p>
+
+
+    </v-flex>
+  </v-layout>
+</template>
